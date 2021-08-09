98443a79359674be2d1e73cb1cbf936f02ee860c
Michael Graham <michaelgraham@mmc.school.nz>
2021-08-09 12:01:59 +1200

    Pinecone affected by gravity.

--- FP91906/Form1.Designer.cs
+++ FP91906/Form1.Designer.cs
@@ -29,36 +29,40 @@ namespace FP91906
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.treeTop = new System.Windows.Forms.PictureBox();
+            this.treeBottom = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pinecone = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.textScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.treeTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.treeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treeBottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pinecone)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // treeTop
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(472, -2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(181, 246);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.treeTop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("treeTop.BackgroundImage")));
+            this.treeTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.treeTop.Location = new System.Drawing.Point(472, -2);
+            this.treeTop.Name = "treeTop";
+            this.treeTop.Size = new System.Drawing.Size(181, 246);
+            this.treeTop.TabIndex = 0;
+            this.treeTop.TabStop = false;
             // 
-            // pictureBox2
+            // treeBottom
             // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(472, 436);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(181, 246);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.treeBottom.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("treeBottom.BackgroundImage")));
+            this.treeBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.treeBottom.Location = new System.Drawing.Point(472, 436);
+            this.treeBottom.Name = "treeBottom";
+            this.treeBottom.Size = new System.Drawing.Size(181, 246);
+            this.treeBottom.TabIndex = 1;
+            this.treeBottom.TabStop = false;
             // 
             // pictureBox3
             // 
@@ -74,11 +78,34 @@ namespace FP91906
             // 
             this.pinecone.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pinecone.BackgroundImage")));
             this.pinecone.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pinecone.Location = new System.Drawing.Point(78, 312);
+            this.pinecone.Location = new System.Drawing.Point(118, 306);
             this.pinecone.Name = "pinecone";
             this.pinecone.Size = new System.Drawing.Size(72, 80);
             this.pinecone.TabIndex = 3;
             this.pinecone.TabStop = false;
+            // 
+            // textScore
+            // 
+            this.textScore.AutoSize = true;
+            this.textScore.BackColor = System.Drawing.Color.Transparent;
+            this.textScore.Font = new System.Drawing.Font("Goudy Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textScore.Location = new System.Drawing.Point(12, 257);
+            this.textScore.Name = "textScore";
+            this.textScore.Size = new System.Drawing.Size(46, 19);
+            this.textScore.TabIndex = 4;
+            this.textScore.Text = "Score:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // treeTimer
+            // 
+            this.treeTimer.Enabled = true;
+            this.treeTimer.Interval = 5;
+            this.treeTimer.Tick += new System.EventHandler(this.treeTimerEvent);
             // 
             // Form1
             // 
@@ -86,26 +113,33 @@ namespace FP91906
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Aqua;
             this.ClientSize = new System.Drawing.Size(702, 752);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pinecone);
+            this.Controls.Add(this.textScore);
+            this.Controls.Add(this.treeTop);
+            this.Controls.Add(this.treeBottom);
             this.Controls.Add(this.pictureBox3);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.treeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treeBottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pinecone)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox treeTop;
+        private System.Windows.Forms.PictureBox treeBottom;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pinecone;
+        private System.Windows.Forms.Label textScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer treeTimer;
     }
 }
 
--- FP91906/Form1.cs
+++ FP91906/Form1.cs
@@ -12,10 +12,36 @@ namespace FP91906
 {
     public partial class Form1 : Form
     {
+        int treeSpeed = 4;
+        int gravity = 15;
+        int score = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            pinecone.Top += gravity;
+        }
+        private void treeTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+        private void gameKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
     }
 }
