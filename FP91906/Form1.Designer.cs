fbeef77653b42c1c8e671b4fdc60fc853c9711a8
Michael Graham <michaelgraham@mmc.school.nz>
2021-09-13 12:29:40 +1200

    Working menu.

--- FP91906/Form1.Designer.cs
+++ FP91906/Form1.Designer.cs
@@ -38,19 +38,21 @@ namespace FP91906
             this.treeTimer = new System.Windows.Forms.Timer(this.components);
             this.treeTop2 = new System.Windows.Forms.PictureBox();
             this.treeBottom2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.RestartBtn = new System.Windows.Forms.Button();
             this.ExitBtn = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreTxt = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.panelShadow = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Countdown = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.treeTop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeBottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pinecone)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeTop2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeBottom2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeTop
@@ -60,7 +62,7 @@ namespace FP91906
             this.treeTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.treeTop.Location = new System.Drawing.Point(449, -236);
             this.treeTop.Name = "treeTop";
-            this.treeTop.Size = new System.Drawing.Size(65, 462);
+            this.treeTop.Size = new System.Drawing.Size(85, 462);
             this.treeTop.TabIndex = 0;
             this.treeTop.TabStop = false;
             // 
@@ -71,7 +73,7 @@ namespace FP91906
             this.treeBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.treeBottom.Location = new System.Drawing.Point(449, 425);
             this.treeBottom.Name = "treeBottom";
-            this.treeBottom.Size = new System.Drawing.Size(65, 501);
+            this.treeBottom.Size = new System.Drawing.Size(85, 501);
             this.treeBottom.TabIndex = 1;
             this.treeBottom.TabStop = false;
             // 
@@ -80,7 +82,7 @@ namespace FP91906
             this.pinecone.BackColor = System.Drawing.Color.Transparent;
             this.pinecone.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pinecone.BackgroundImage")));
             this.pinecone.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pinecone.Location = new System.Drawing.Point(118, 306);
+            this.pinecone.Location = new System.Drawing.Point(117, 305);
             this.pinecone.Name = "pinecone";
             this.pinecone.Size = new System.Drawing.Size(72, 80);
             this.pinecone.TabIndex = 3;
@@ -89,7 +91,7 @@ namespace FP91906
             // gameTimer
             // 
             this.gameTimer.Enabled = true;
-            this.gameTimer.Interval = 25;
+            this.gameTimer.Interval = 22;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
             // 
             // treeTimer
@@ -103,9 +105,9 @@ namespace FP91906
             this.treeTop2.BackColor = System.Drawing.Color.Transparent;
             this.treeTop2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("treeTop2.BackgroundImage")));
             this.treeTop2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.treeTop2.Location = new System.Drawing.Point(929, -236);
+            this.treeTop2.Location = new System.Drawing.Point(926, -280);
             this.treeTop2.Name = "treeTop2";
-            this.treeTop2.Size = new System.Drawing.Size(65, 462);
+            this.treeTop2.Size = new System.Drawing.Size(85, 462);
             this.treeTop2.TabIndex = 5;
             this.treeTop2.TabStop = false;
             // 
@@ -114,44 +116,11 @@ namespace FP91906
             this.treeBottom2.BackColor = System.Drawing.Color.Transparent;
             this.treeBottom2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("treeBottom2.BackgroundImage")));
             this.treeBottom2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.treeBottom2.Location = new System.Drawing.Point(929, 425);
+            this.treeBottom2.Location = new System.Drawing.Point(926, 381);
             this.treeBottom2.Name = "treeBottom2";
-            this.treeBottom2.Size = new System.Drawing.Size(65, 501);
+            this.treeBottom2.Size = new System.Drawing.Size(85, 501);
             this.treeBottom2.TabIndex = 6;
             this.treeBottom2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(437, 107);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(65, 462);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(917, 768);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(65, 501);
-            this.pictureBox2.TabIndex = 6;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(917, 107);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(65, 462);
-            this.pictureBox3.TabIndex = 5;
-            this.pictureBox3.TabStop = false;
             // 
             // RestartBtn
             // 
@@ -164,12 +133,13 @@ namespace FP91906
             this.RestartBtn.TabIndex = 7;
             this.RestartBtn.Text = "Restart";
             this.RestartBtn.UseVisualStyleBackColor = false;
+            this.RestartBtn.Visible = false;
             this.RestartBtn.Click += new System.EventHandler(this.RestartBtn_Click);
             // 
             // ExitBtn
             // 
             this.ExitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(238)))), ((int)(((byte)(184)))));
-            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.ExitBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ExitBtn.Location = new System.Drawing.Point(549, 545);
             this.ExitBtn.Name = "ExitBtn";
@@ -177,7 +147,74 @@ namespace FP91906
             this.ExitBtn.TabIndex = 8;
             this.ExitBtn.Text = "Exit";
             this.ExitBtn.UseVisualStyleBackColor = false;
+            this.ExitBtn.Visible = false;
             this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.White;
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.scoreTxt);
+            this.panel.Controls.Add(this.title);
+            this.panel.Location = new System.Drawing.Point(218, 125);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(531, 493);
+            this.panel.TabIndex = 9;
+            this.panel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(202, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "80";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreTxt
+            // 
+            this.scoreTxt.AutoSize = true;
+            this.scoreTxt.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scoreTxt.Location = new System.Drawing.Point(66, 240);
+            this.scoreTxt.Name = "scoreTxt";
+            this.scoreTxt.Size = new System.Drawing.Size(119, 37);
+            this.scoreTxt.TabIndex = 1;
+            this.scoreTxt.Text = "Score:";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Arial Rounded MT Bold", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(39, 35);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(450, 61);
+            this.title.TabIndex = 0;
+            this.title.Text = "Flappy Pinecone";
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.Color.Silver;
+            this.panelShadow.Location = new System.Drawing.Point(211, 132);
+            this.panelShadow.Name = "panelShadow";
+            this.panelShadow.Size = new System.Drawing.Size(531, 493);
+            this.panelShadow.TabIndex = 10;
+            this.panelShadow.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(779, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "3";
+            // 
+            // Countdown
+            // 
+            this.Countdown.Interval = 1000;
+            this.Countdown.Tick += new System.EventHandler(this.Countdown_Tick);
             // 
             // Form1
             // 
@@ -186,8 +223,11 @@ namespace FP91906
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(961, 735);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.ExitBtn);
             this.Controls.Add(this.RestartBtn);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.panelShadow);
             this.Controls.Add(this.pinecone);
             this.Controls.Add(this.treeTop2);
             this.Controls.Add(this.treeBottom2);
@@ -196,16 +236,15 @@ namespace FP91906
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
-            //this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsUp);
             ((System.ComponentModel.ISupportInitialize)(this.treeTop)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeBottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pinecone)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeTop2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.treeBottom2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -218,11 +257,15 @@ namespace FP91906
         private System.Windows.Forms.Timer treeTimer;
         private System.Windows.Forms.PictureBox treeTop2;
         private System.Windows.Forms.PictureBox treeBottom2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button RestartBtn;
         private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Panel panelShadow;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label scoreTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer Countdown;
     }
 }
 
--- FP91906/Form1.cs
+++ FP91906/Form1.cs
@@ -16,21 +16,20 @@ namespace FP91906
         int velocity = 0;
         int acceleration = 1;
 
-        int treeSpeed = 4;
+        int timer = 3;
+        readonly int treeSpeed = 4;
         int score = 0;
-        int gap = 200;
+        readonly int gap = 200;
 
         public Form1()
         {
-            InitializeComponent();
-            RestartBtn.Visible = false;
-            ExitBtn.Visible = false;
+            InitializeComponent();     
         }
         private void gameTimerEvent(object sender, EventArgs e)
         {
             pinecone.Top += velocity;
             velocity += acceleration;
-            
+
             if (pinecone.Bounds.IntersectsWith(treeBottom.Bounds) ||
                 pinecone.Bounds.IntersectsWith(treeTop.Bounds) ||
                 pinecone.Bounds.IntersectsWith(treeBottom2.Bounds) ||
@@ -39,13 +38,12 @@ namespace FP91906
                 pinecone.Top + pinecone.Height > ClientSize.Height)
             {
                 EndGame();
-            }          
+            }
         }
         private void treeTimerEvent(object sender, EventArgs e)
         {
             Random rnd = new Random();
             Random rnd2 = new Random();
-
 
             treeBottom.Left -= treeSpeed;
             treeTop.Left -= treeSpeed;
@@ -53,21 +51,16 @@ namespace FP91906
             treeBottom2.Left -= treeSpeed;
             treeTop2.Left -= treeSpeed;
 
-            //if () // decrease gap
-            //{
-            //-
-            //}
-
             if (treeTop.Left <= -70)
             {
                 treeTop.Left = 977;
-                treeTop.Top = rnd.Next(-453, 23);
+                treeTop.Top = rnd.Next(-400, 0);
                 score++;
             }
             if (treeTop2.Left <= -70)
             {
                 treeTop2.Left = 977;
-                treeTop2.Top = rnd2.Next(-453, 23);
+                treeTop2.Top = rnd2.Next(-400, 0);
                 score++;
             }
             if (treeBottom.Left <= -70)
@@ -83,137 +76,89 @@ namespace FP91906
         }
         private void gameKeyIsDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Space)
+            if (pinecone.Top > 0 || pinecone.Top + pinecone.Height == ClientSize.Height)
             {
-                velocity = -12;
-            }
-            if (e.KeyCode == Keys.C)
-            {
-                velocity = 0;
-                pinecone.Top = treeTop.Top + treeTop.Height + gap/2 - pinecone.Height/2;
-                if (treeTop2.Left < treeTop.Left)
+                if (e.KeyCode == Keys.Space)
                 {
-                    pinecone.Top = treeTop2.Top + treeTop2.Height + gap / 2 - pinecone.Height / 2;
+                    velocity = -12;
                 }
-            }
+            }           
         }
-        //private void gameKeyIsUp(object sender, KeyEventArgs e)
-        //{
-        //    if (e.KeyCode == Keys.Space)
-        //    {
-        //        velocity = 10;
-        //    }
-        //    if (e.KeyCode == Keys.C)
-        //    {
-        //        velocity = 0;
-        //    }
-        //}
-        private void EndGame()
+        public void ShowMenu()
         {
-            gameTimer.Stop();
-            treeTimer.Stop();
             pinecone.Visible = false;
             treeTop.Visible = false;
             treeTop2.Visible = false;
             treeBottom.Visible = false;
             treeBottom2.Visible = false;
 
+            title.Visible = true;
+            panel.Visible = true;
+            panelShadow.Visible = true;
+            scoreTxt.Visible = true;
             RestartBtn.Visible = true;
+            RestartBtn.BringToFront();
             ExitBtn.Visible = true;
+            ExitBtn.BringToFront();
 
-            Menu();
+            label1.Text = score.ToString();
         }
-        public void Menu()
-        { 
-            //Back Panel
-            Panel panel = new Panel
-            {
-                Size = new System.Drawing.Size(531, 493),
-                BackColor = Color.White,
-                Location = new Point(209, 120),
-            };
-            ;
-            this.Controls.Add(panel);
-
-            //Shadow Panel
-            Panel Shadow = new Panel
-            {
-                Size = new System.Drawing.Size(531, 493),
-                BackColor = Color.Gray,
-                Location = new Point(204, 125),
-            };
-            ;
-            this.Controls.Add(Shadow);
-            Shadow.SendToBack();
-
-            //Title Label
-            Label Title = new Label
-            {
-                Size = new System.Drawing.Size(531, 100),
-                BackColor = Color.White,
-                Location = new Point(panel.Location.X, panel.Location.Y + 25),
-                BorderStyle = BorderStyle.None,
-                Text = "Flappy Pinecone",
-                TextAlign = ContentAlignment.MiddleCenter,
-                ForeColor = Color.Black,
-                Font = new Font("Arial Rounded MT Bold", 40)
-            };
-            ;
-            this.Controls.Add(Title);
-            Title.BringToFront();
-
-            //Score: Text
-            Label ScoreTxt = new Label
-            {
-                Size = new System.Drawing.Size(115, 115),
-                BackColor = Color.White,
-                Location = new Point(284, 270),
-                Text = "Score:",
-                TextAlign = ContentAlignment.MiddleCenter,
-                ForeColor = Color.Black,
-                Font = new Font("Arial Rounded MT", 15)
-            };
-            this.Controls.Add(ScoreTxt);
-            ScoreTxt.BringToFront();
-
-            //Score Text
-            Label ScoreNum = new Label
-            {
-                Size = new System.Drawing.Size(115, 115),
-                BackColor = Color.White,
-                Location = new Point(284, 350),
-                Text = score.ToString(),
-                TextAlign = ContentAlignment.MiddleCenter,
-                ForeColor = Color.Black,
-                Font = new Font("Arial Rounded MT Bold", 50)
-            };
-            ;      
-            this.Controls.Add(ScoreNum);
-            ScoreNum.BringToFront();      
-        }
-
-        public void RestartBtn_Click(object sender, EventArgs e)
+        public void HideMenu()
         {
-            pinecone.Top = ClientSize.Height / 2;
-            gameTimer.Start();
-            treeTimer.Start();
             pinecone.Visible = true;
             treeTop.Visible = true;
-            treeTop.Left = 449;
             treeTop2.Visible = true;
-            treeTop2.Left = 449;
             treeBottom.Visible = true;
-            treeBottom.Left = 929;
             treeBottom2.Visible = true;
-            treeBottom2.Left = 929;
- 
-            RestartBtn.Visible = false;
-            //this.Controls.Remove(panel);
 
+            treeTop.Left = treeBottom.Left = 449;
+            treeTop2.Left = treeBottom2.Left = 949;
+            treeTop.Top = -236;
+            treeTop2.Top = -280;
+            treeBottom.Top = treeTop.Top + treeTop.Height + gap;
+            treeBottom2.Top = treeTop2.Top + treeTop2.Height + gap;
+
+            title.Visible = false;
+            panel.Visible = false;
+            panelShadow.Visible = false;
+            scoreTxt.Visible = false;
+            RestartBtn.Visible = false;
+            ExitBtn.Visible = false;
+
+            
+
+            velocity = 0;
+            acceleration = 1;
+       
+            pinecone.Top = ClientSize.Height / 2;
+
+
+            this.Focus();
+            gameTimer.Start();
+            treeTimer.Start();
+        }
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            treeTimer.Stop();
+            
+            velocity = 0;
+            acceleration = 0;
+      
+            ShowMenu();
+        }
+        public void RestartBtn_Click(object sender, System.EventArgs e)
+        {
+            HideMenu();
         }
         private void ExitBtn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void Countdown_Tick(object sender, EventArgs e)
+        {
+            timer--;
+            label2.Text = timer.ToString();
         }
     }
 }
